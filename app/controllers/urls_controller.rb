f69f1ec4a2d9d2e666fa13ca0dc5f7c2de08ef8d
WEI MUN SIEW <cherylsiew-macbpro@WEIs-MacBook-Pro.local>
2017-05-26 14:37:06 +0800

    done

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -6,4 +6,35 @@ end
 def show
     @url = Url.find(params[:id])
 end
+
+def new
+  @url = Url.new
+end
+
+def create
+@url = Url.new(url_params)
+  @url.shorten
+  if @url.save
+      redirect_to @url  
+  else
+      render 'new'
+  end   
+end
+
+def short
+  url = Url.find(params[:id])
+  redirect_to url.long_url
+end
+
+def destroy
+    @url = Url.find(params[:id])
+    @url.destroy
+    redirect_to urls_path
+end
+
+private 
+def url_params
+    params.require(:url).permit(:long_url)
+end
+
 end
